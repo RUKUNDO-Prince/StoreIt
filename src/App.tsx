53ade0a56b5a98be7b6beb7ca44dbc40563aa1c0
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-01 19:57:52 +0200

    some modifications

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import "./globals.css";
 import Home from './_root/pages/Home';
 import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         {/* PUBLIC ROUTES => ACCESSIBLE TO EVERYONE */}
         <Route path='/sign-in' element={<SigninForm />} />
+        <Route path='/sign-up' element={<SignupForm />} />
 
         {/* PRIVATE ROUTES => ACCESSIBLE TO SIGNED IN USERS */}
         <Route index element={<Home />} />
