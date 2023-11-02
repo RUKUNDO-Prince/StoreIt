d7ad23219c234304c31bab91f67105c511f54f62
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-02 19:32:37 +0200

    added more routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import "./globals.css";
 import { Home } from './_root/pages';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
 
 const App = () => {
   return (
     <main className='flex h-screen'>
       <Routes>
         {/* PUBLIC ROUTES => ACCESSIBLE TO EVERYONE */}
-        <Route path='/sign-in' element={<SigninForm />} />
-        <Route path='/sign-up' element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
 
         {/* PRIVATE ROUTES => ACCESSIBLE TO SIGNED IN USERS */}
         <Route index element={<Home />} />
