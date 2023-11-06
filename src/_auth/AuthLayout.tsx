cb5d69a6f8ca2355634bc60684ff2caffe706c0d
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-06 20:34:19 +0200

    successfull added the button from shadcn

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
+  
+
   return (
-    <div>
-      AuthLayout
-    </div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <Outlet />
+        </>
+      )}
+    </>
   )
 }
 
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { Button } from "@/components/ui/button"
+
+// export default function Home() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+
 const SignupForm = () => {
   return (
     <div>
       Sign Up
+      <Button>Click me</Button>
     </div>
   )
 }
