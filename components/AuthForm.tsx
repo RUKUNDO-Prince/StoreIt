742eaefcc695c3d1372b2ea2d0779f6cab07e56b
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-01-07 11:30:57 +0200

    finished about sending otp and creating account

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import {
 import { Input } from '@/components/ui/input';
 import Image from 'next/image';
 import Link from 'next/link';
+import { createAccount } from '@/lib/actions/user.actions';
 
 type FormType = "sign-in" | "sign-up"
 
@@ -30,6 +31,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState(null)
 
   const formSchema = authFormSchema(type);
 
@@ -42,7 +44,22 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const user = await createAccount({
+        fullName: values.fullName || "",
+        email: values.email,
+      });
+  
+      setAccountId(user.accountId);
+    } catch (error) {
+      setErrorMessage("Failed to create account. Please try again later!");
+    } finally {
+      setIsLoading(false);
+    }
+
   }
 
   return (
