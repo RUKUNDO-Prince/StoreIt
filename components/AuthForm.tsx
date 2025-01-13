745757a3642c3c5631fffac8b51136f04a47c47e
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-01-13 05:43:02 +0200

    finished all about authentication

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useState } from 'react'
-import { z } from "zod"
-import { zodResolver }  from "@hookform/resolvers/zod"
-import { useForm } from 'react-hook-form';
-import { Button } from '@/components/ui/button';
+import React, { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,27 +12,30 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage
+  FormMessage,
 } from "@/components/ui/form";
-import { Input } from '@/components/ui/input';
-import Image from 'next/image';
-import Link from 'next/link';
-import { createAccount } from '@/lib/actions/user.actions';
-import OTPModal from './OTPModal';
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+import { createAccount, signInUser } from "@/lib/actions/user.actions";
+import OTPModal from "./OTPModal";
 
-type FormType = "sign-in" | "sign-up"
+type FormType = "sign-in" | "sign-up";
 
 const authFormSchema = (formType: FormType) => {
   return z.object({
     email: z.string().email(),
-    fullName: formType === "sign-up" ? z.string().min(2).max(50) : z.string().optional(),
-  })
-}
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const [accountId, setAccountId] = useState(null)
+  const [accountId, setAccountId] = useState(null);
 
   const formSchema = authFormSchema(type);
 
@@ -40,7 +43,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       fullName: "",
-      email: ""
+      email: "",
     },
   });
 
@@ -49,75 +52,108 @@ const AuthForm = ({ type }: { type: FormType }) => {
     setErrorMessage("");
 
     try {
-      const user = await createAccount({
-        fullName: values.fullName || "",
-        email: values.email,
-      });
-  
+      const user =
+        type === "sign-up"
+          ? await createAccount({
+              fullName: values.fullName || "",
+              email: values.email,
+            })
+          : await signInUser({ email: values.email });
+
       setAccountId(user.accountId);
     } catch (error) {
       setErrorMessage("Failed to create account. Please try again later!");
     } finally {
       setIsLoading(false);
     }
-
-  }
+  };
 
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='auth-form'>
-          <h1 className='form-title'>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
             {type === "sign-up" ? "Sign Up" : "Sign In"}
           </h1>
-          {type === "sign-up" && <FormField
-            control={form.control}
-            name="fullName"
-            render={({ field }) => (
-              <FormItem>
-                <div className='shad-form-item'>
-                  <FormLabel className='shad-form-label'>Full Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder='Enter Your Full Name' className='shad-input' {...field} />
-                  </FormControl>
-                </div>
-                <FormMessage className='shad-form-message' />
-              </FormItem>
-            )}
-          />}
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter Your Full Name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
 
           <FormField
             control={form.control}
             name="email"
             render={({ field }) => (
               <FormItem>
-                <div className='shad-form-item'>
-                  <FormLabel className='shad-form-label'>Email</FormLabel>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
                   <FormControl>
-                    <Input placeholder='Enter Your Email' className='shad-input' {...field} />
+                    <Input
+                      placeholder="Enter Your Email"
+                      className="shad-input"
+                      {...field}
+                    />
                   </FormControl>
                 </div>
-                <FormMessage className='shad-form-message' />
+                <FormMessage className="shad-form-message" />
               </FormItem>
             )}
           />
 
-          <Button type='submit' className='form-submit-button' disabled={isLoading}>
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
             {type === "sign-up" ? "Sign Up" : "Sign In"}
-            {isLoading && <Image src="/assets/icons/loader.svg" alt='loader' width={24} height={24} className='ml-2 animate-spin' />}
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
           </Button>
-          {errorMessage && <p className='error-message'>*{errorMessage}</p>}
-          <div className='body-2 flex justify-center'>
-            <p className='text-light-100'>
-              {type === "sign-up" ? "Already have an account?" : "Don't have an account?"}
+          {errorMessage && <p className="error-message">*{errorMessage}</p>}
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === "sign-up"
+                ? "Already have an account?"
+                : "Don't have an account?"}
             </p>
-            <Link href={type === "sign-up" ? "/sign-in" : "/sign-up"} className='ml-1 font-medium text-brand'>{type === "sign-up" ? "Sign In" : "Sign Up"}</Link>
+            <Link
+              href={type === "sign-up" ? "/sign-in" : "/sign-up"}
+              className="ml-1 font-medium text-brand"
+            >
+              {type === "sign-up" ? "Sign In" : "Sign Up"}
+            </Link>
           </div>
         </form>
       </Form>
-      {accountId && <OTPModal email={form.getValues('email')} accountId={accountId} />}
+      {accountId && (
+        <OTPModal email={form.getValues("email")} accountId={accountId} />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default AuthForm
+export default AuthForm;
--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -17,6 +17,7 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { Button } from './ui/button';
 import FileUploader from './FileUploader';
+import { signOutUser } from '@/lib/actions/user.actions';
 
 interface Props {
     fullName: string,
@@ -64,7 +65,7 @@ const MobileNavigation = ({ fullName, email, avatar, ownerId, accountId }: Props
                 <Separator className="my-5 bg-light-200/20" />
                 <div className='flex flex-col justify-between gap-5 pb-5'>
                     <FileUploader />
-                    <Button type='submit' className='mobile-sign-out-button' onClick={() => {}}>
+                    <Button type='submit' className='mobile-sign-out-button' onClick={async () => await signOutUser()}>
                         <Image src="/assets/icons/logout.svg" alt='logout' width={24} height={24} className='' />
                         <p>Logout</p>
                     </Button>
