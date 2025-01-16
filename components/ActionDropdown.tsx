a20ef972f1a0f881eb7dd01af2f42b255de2fc44
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-01-16 08:19:35 +0200

    successfully displaying the file details

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -19,6 +19,7 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { renameFile } from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
+import { FileDetails } from "./ActionModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -63,6 +64,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
           {value === "rename" && (
             <Input type="text" value={name} onChange={(e) => setName(e.target.value)} />
           )}
+          {value === "details" && <FileDetails file={file} />}
         </DialogHeader>
         {['rename', 'delete', 'share'].includes(value) && (
             <DialogFooter className="flex flex-col gap-3 md:flex-row">
